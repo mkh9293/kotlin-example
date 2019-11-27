2f2abaff10b6416ff4016ddf896d02ebb744a22e
GwangHun Moon <mkh9293@gmail.com>
2019-11-27 23:56:11 +0900

    [Update] chapter2

---
+++ src/TypeIntro.kt
@@ -0,0 +1,9 @@
+const val MAX_EXPERIENCE = 500
+
+fun main( args: Array<String>) {
+    val playerName = "에스트라곤"
+    var experiencePoints = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+}
---
+++ src/chapter2/Challenge.kt
@@ -0,0 +1,12 @@
+package chapter2
+
+fun main(args: Array<String>) {
+    var playerName = "에스트라곤"
+    var hasSteed = false
+    var barName = "Unicorns's Horn"
+    var barOwner = "owner"
+    var coin = 50
+    var barMenu = listOf("Honey wine", "wine", "racrua")
+
+    println(playerName.reversed())
+}
