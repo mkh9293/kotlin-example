0e9f3a6e7de8afbad02c6c8155a71c771cbef08c
GwangHun Moon <mkh9293@gmail.com>
2020-01-23 22:47:10 +0900

    Update chapter18
    ing...

--- src/chapter18/Extentions.kt
+++ src/chapter18/Extentions.kt
@@ -1,6 +1,11 @@
 package chapter18
 
-fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
+infix fun String?.printWithDefault(default: String) = print(this ?: default)
+
+val String.numVowels
+    get() = count {"aeiouy".contains(it)}
+
+fun String?.addEnthusiasm(amount: Int = 1) = (this?: "1") + "!".repeat(amount)
 
 fun <T> T.easyPrint() : T {
     println(this)
@@ -11,4 +16,8 @@ fun main() {
 //    println("마드리갈이 그 건물에서 나왔습니다".addEnthusiasm())
     "마드리갈이 그 건물에서 나왔습니다".easyPrint().addEnthusiasm().easyPrint()
     42.easyPrint()
+    "How many vowels?".numVowels.easyPrint()
+
+    val nullableString: String? = null
+    nullableString printWithDefault "기본 문자열"
 }
