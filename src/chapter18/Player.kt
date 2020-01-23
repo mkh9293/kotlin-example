f1b0b7213371a2152ed922498db4649b5be22627
GwangHun Moon <mkh9293@gmail.com>
2020-01-24 03:23:49 +0900

    Update chapter18

---
+++ src/chapter18/Player.kt
@@ -0,0 +1,55 @@
+package chapter18
+
+import extensions.random as randomizer
+import java.io.File
+
+class Player3(_name: String,
+              var healthPoints: Int,
+              val isBlessed: Boolean,
+              private val isImmotal: Boolean) {
+    var name = _name
+        get() = "${field.capitalize()} of $hometown"
+        private set(value) {
+            field = value.trim()
+        }
+
+    val hometown by lazy { selectHometown() }
+
+    private fun selectHometown() = File("data/towns.txt")
+                                    .readText()
+                                    .split("\r\n")
+                                    .randomizer()
+
+    init {
+        require(healthPoints > 0, {"healthPoints는 0보다 커야 합니다."})
+        require(name.isNotBlank(), {"플레이어는 이름이 있어야 합니다."})
+    }
+
+    constructor(name: String) : this (name, healthPoints= 100, isBlessed = true, isImmotal = false) {
+        if (name.toLowerCase() == "kar") healthPoints = 40
+    }
+
+    fun castFireball(numFireballs: Int = 2) =
+        println("한 덩어리의 파이어볼이 나타난다. (x$numFireballs)")
+
+    fun auraColor() =
+        if (isBlessed && healthPoints > 50 || isImmotal) "GREEN" else "NONE"
+
+    fun formatHealthStatus() =
+        when (healthPoints) {
+            100 -> " 최상의 상태임!"
+            in 90..99 -> "약간의 찰과상만 있음."
+            in 75..89 -> if (isBlessed) {
+                "경미한 상처가 있지만 빨리 치유되고 있음!"
+            } else {
+                "경미한 상처만 있음."
+            }
+            in 15..74 -> "많이 다친 것 같음."
+            else -> "최악의 상태임!"
+        }
+}
+
+fun main() {
+    val player2 = Player3("Madrigal", healthPoints =100, isBlessed = true, isImmotal = false)
+    player2.castFireball()
+}
---
+++ src/chapter18/Tavern.kt
@@ -0,0 +1,44 @@
+package chapter18
+
+import java.io.File
+
+const val TAVERN_NAME = "Taernyl's Folly"
+
+val patronList: List<String> = listOf("Eli", "Mordoc", "Sopie")
+val lastName = listOf("Ironfoot", "Fernsworh", "Baggins")
+var menuList = File("data/tavern-menu-items.txt").readText().split("\r\n")
+
+val uniquePatrons = mutableSetOf<String>()
+fun main() {
+    (0..9).forEach {index ->
+        val first = patronList.random()
+        val last = lastName.random()
+        val name = "$first $last"
+        uniquePatrons += name
+    }
+    println(uniquePatrons)
+
+    var orderCount = 0
+    while(orderCount <= 9) {
+        placeOrder(uniquePatrons.random(), menuList.random())
+        orderCount++
+    }
+
+}
+
+fun placeOrder(patronName: String, menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("$patronName 은 $tavernMaster 에게 주문한다.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "$patronName 은 금화 $price 로 $name ($type)를 구입한다."
+    println(message)
+
+    val phrase = if (name == "Dragon's Breath") {
+        "$patronName 이 감탄한다: 와, $name 진짜 좋구나!"
+    } else {
+        "$patronName 이 말한다: 감사합니다 $name."
+    }
+    println(phrase)
+}
